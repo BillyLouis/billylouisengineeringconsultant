e6a0b0299f039c00fbbd6833bd5ba205d6c25213
Billy Louis <louis.office2011@gmail.com>
2026-03-26 22:34:34 -0700

    Added Certs and Privacy Policies -c2.032626

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,6 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function showCert(path) {
+    document.getElementById("certFrame").src = path;
+}
